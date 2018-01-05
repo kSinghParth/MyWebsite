d3f98aaf115bc89b9316ba05a78bbba1b65d5df2
Kunwar Parth Singh <parthsingh287@icloud.com>
2018-01-06 01:02:36 +0530

    Adding horizontal paroller scroll to each block in an alternate fashion

--- MW1/JS/dist.js
+++ MW1/JS/dist.js
@@ -1,8 +1,6 @@
-$(function () {
-	$("body").paroller();
-	$("#comp2").paroller({ factor: '1.0', type: 'foreground', direction: 'horizontal' });
-	$(".codesnip").paroller({ factor: '0.1', type: 'foreground', direction: 'horizontal' });
-	$("#comp0 img").paroller({ factor: '-0.7', type: 'foreground', direction: 'horizontal' });
-	$("h1").paroller({ factor: '-0.7', type: 'foreground', direction: 'horizontal' });
 
-})
+
+
+
+
+
